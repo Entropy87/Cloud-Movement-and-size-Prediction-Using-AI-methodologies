1104772640838bded58d8e9a31e0cd4926ac45f9
Entropy87 <thamsanqamashinini01@gmail.com>
2024-10-17 00:41:20 +0200

    Trajectory prediction

---
+++ kalmanfilter.py
@@ -0,0 +1,20 @@
+#https://pysource.com/2021/10/29/kalman-filter-predict-the-trajectory-of-an-object/
+import cv2
+import numpy as np
+
+
+# Define the Kalman Filter class
+class KalmanFilter:
+    def __init__(self):
+        self.kf = cv2.KalmanFilter(4, 2)
+        self.kf.measurementMatrix = np.array([[1, 0, 0, 0], [0, 1, 0, 0]], np.float32)
+        self.kf.transitionMatrix = np.array([[1, 0, 1, 0], [0, 1, 0, 1], [0, 0, 1, 0], [0, 0, 0, 1]], np.float32)
+
+    def predict(self, coordX, coordY):
+        ''' This function estimates the position of the object'''
+        measured = np.array([[np.float32(coordX)], [np.float32(coordY)]])
+        self.kf.correct(measured)
+        predicted = self.kf.predict()
+        x = int(predicted[0, 0])
+        y = int(predicted[1, 0])
+        return x, y
